c32b8576d05c100d8a18e7975d90031894e73c1d
Sydney Waters <saw465712@uga.edu>
2019-11-25 14:15:36 -0500

    Initial Commit

---
+++ Closet_1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Closet_1
+//
+//  Created by Sydney Ann Waters on 11/25/19.
+//  Copyright © 2019 Sydney Ann Waters. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
